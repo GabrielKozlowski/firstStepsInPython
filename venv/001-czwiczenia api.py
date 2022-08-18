361b376908433b5d216b881e9adc976432482a36
Gabriel <gabriel89krk@gmail.com>
2022-08-18 20:51:47 +0200

    zmiana kosmetyczna

--- venv/001-czwiczenia api.py
+++ venv/001-czwiczenia api.py
@@ -42,18 +42,22 @@
 #
 # ---------------------------------------------------------------------------------
 #***Zadanie pobrania 5 losowych zdjęć psów i otwarcie ich w przeglądarce***
+#
+# import requests
+# import json
+# import webbrowser
+#
+# r = requests.get("https://dog.ceo/api/breeds/image/random/5")
+#
+#
+# try:
+#     photo = r.json()
+# except json.decoder.JSONDecodeError:
+#     print("Bad format")
+# else:
+#     for pictur in photo['message']:
+#         webbrowser.open(pictur)
+#
+# ------------------------------------------------------------------------
 
-import requests
-import json
-import webbrowser
 
-r = requests.get("https://dog.ceo/api/breeds/image/random/5")
-
-
-try:
-    photo = r.json()
-except json.decoder.JSONDecodeError:
-    print("Bad format")
-else:
-    for pictur in photo['message']:
-        webbrowser.open(pictur)
