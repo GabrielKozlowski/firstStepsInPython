85ea2e4ee7007731c18886f7b8dc5fb14435cb45
Gabriel <gabriel89krk@gmail.com>
2022-08-26 16:00:45 +0200

    dalsze ćwiczenia

--- venv/002-ćwiczenia programowania obiektowego.py
+++ venv/002-ćwiczenia programowania obiektowego.py
@@ -1,61 +1,83 @@
-"""
-OOP - Object Oriented Programming
-
-Programowanie zorientowane wokół obiektów
-
-OBIEKT
-
-OBIEKTY - to pojemniki do przechowywania zmiennych i funkcji tematycznnie ze
-          sobą powiązanych do dalszego łatwiejszego ponownego użycia
-
-Klasy - foremki (szablony) do tworzenia egzemplarzy obiektów
-
-Atrybuty - cecha opisująca obiekt
-Metoda - funkcja, która operuje na obiekcie
-
-Instancja klasy - instancja  ang. egzemplarz to obiekt, który wyszedł z formy (klasy)
-
-
-__init__ - initialization - inicjalizacja - zyli ustawienie startowych
-           wartości dla atrybutów
-
-w innych językach __init__ to konstruktor
-
-"""
-
-
-"""
--klasa , stwórz rakietę która leci do góry.
--stwórz atrybuty i metody
--stwórz 5 instancji /klasy /obiektów "rocket" o startowej wysokości 0
--porusz wszystkie rakiety 10x losowo do góry i wypisz wysokości wszystkich rakiet
-
-"""
-
-
-
-
-import random
+# """
+# OOP - Object Oriented Programming
+#
+# Programowanie zorientowane wokół obiektów
+#
+# OBIEKT
+#
+# OBIEKTY - to pojemniki do przechowywania zmiennych i funkcji tematycznnie ze
+#           sobą powiązanych do dalszego łatwiejszego ponownego użycia
+#
+# Klasy - foremki (szablony) do tworzenia egzemplarzy obiektów
+#
+# Atrybuty - cecha opisująca obiekt
+# Metoda - funkcja, która operuje na obiekcie
+#
+# Instancja klasy - instancja  ang. egzemplarz to obiekt, który wyszedł z formy (klasy)
+#
+#
+# __init__ - initialization - inicjalizacja - zyli ustawienie startowych
+#            wartości dla atrybutów
+#
+# w innych językach __init__ to konstruktor
+#
+# """
+#
+#
+# """
+# -klasa , stwórz rakietę która leci do góry.
+# -stwórz atrybuty i metody
+# -stwórz 5 instancji /klasy /obiektów "rocket" o startowej wysokości 0
+# -porusz wszystkie rakiety 10x losowo do góry i wypisz wysokości wszystkich rakiet
+#
+# """
+#
+#
+#
+#
+# import random
+# class Rocket:
+#
+#     def __init__(self):
+#
+#         self.height = 0
+#
+#     def move_up(self):
+#
+#         self.height += 1
+#
+#
+#
+# rockets = [Rocket() for _ in range(5)]
+#
+# for _ in range(10):
+#     rocketIndexToMove = random.randint(0, 4)
+#     rockets[rocketIndexToMove].move_up()
+#
+#
+# for rocket in rockets:
+#     print(rocket.height)
+# ##-----------------------------------------------------------------------
+#
+from random import randint
 class Rocket:
 
     def __init__(self):
 
-        self.height = 0
+        self.altitude = 0
 
-    def move_up(self):
+    def moveUp(self):
 
-        self.height += 1
-
+        self.altitude += 1
 
 
 rockets = [Rocket() for _ in range(5)]
 
 for _ in range(10):
-    rocketIndexToMove = random.randint(0, 4)
-    rockets[rocketIndexToMove].move_up()
-
+    rocketIndexToMove = randint(0,4)
+    rockets[rocketIndexToMove].moveUp()
 
 for rocket in rockets:
-    print(rocket.height)
+    print(rocket.altitude)
 
 
